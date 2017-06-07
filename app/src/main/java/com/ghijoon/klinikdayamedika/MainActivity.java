67b947e45e15ffd8c11a4f10a446aeb04e138d43
Rendi K <ghijoon@gmail.com>
2017-06-07 11:43:49 +0700

    [UPDATE] About Activity

--- app/src/main/java/com/ghijoon/klinikdayamedika/MainActivity.java
+++ app/src/main/java/com/ghijoon/klinikdayamedika/MainActivity.java
@@ -1,9 +1,23 @@
 package com.ghijoon.klinikdayamedika;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void btnCariRuteOnClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void btnTentangOnClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
