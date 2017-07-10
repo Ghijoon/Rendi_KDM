e03ed67b3d1fa1bf16c5d1c403aac96720885385
Rendi K <ghijoon@gmail.com>
2017-07-11 06:42:33 +0700

    Add Djikstra Java Files

---
+++ app/src/main/java/com/ghijoon/klinikdayamedika/HitungBobotTambahSimpul.java
@@ -0,0 +1,75 @@
+package com.ghijoon.klinikdayamedika;
+
+/**
+ * Created by Ghijoon on 01-Jul-17.
+ */
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.location.Location;
+
+public class HitungBobotTambahSimpul {
+    double bobot = 0;
+
+    public void Hitung_Bobot_Tambah_Simpul(int index, int limit, JSONArray jArrCoordinates) throws JSONException{
+
+        // cuma dijalanin sekali, kalo limit 1, maka 1-0 = 0
+        // 0 == 0 (limit)
+        if(index == limit){
+            // get JSON coordinate
+            JSONArray latlngs = jArrCoordinates.getJSONArray(index);
+
+            double lat_0 = latlngs.getDouble(0);
+            double lng_0 = latlngs.getDouble(1);
+
+            Location simpulAwal = new Location("");
+            simpulAwal.setLatitude(lat_0);
+            simpulAwal.setLongitude(lng_0);
+
+            // get coordinate again
+            JSONArray latlngs1 = jArrCoordinates.getJSONArray(++index);
+
+            double lat_1 = latlngs1.getDouble(0);
+            double lng_1 = latlngs1.getDouble(1);
+
+            Location simpulTengah = new Location("");
+            simpulTengah.setLatitude(lat_1);
+            simpulTengah.setLongitude(lng_1);
+
+            //simpan jarak
+            bobot += simpulAwal.distanceTo(simpulTengah);
+
+        }else{
+            for(int i = 0; i < 1; i++){
+
+                // get JSON coordinate
+                JSONArray latlngs = jArrCoordinates.getJSONArray(index);
+
+                double lat_0 = latlngs.getDouble(0);
+                double lng_0 = latlngs.getDouble(1);
+
+                Location simpulAwal = new Location("");
+                simpulAwal.setLatitude(lat_0);
+                simpulAwal.setLongitude(lng_0);
+
+                // get coordinate again
+                JSONArray latlngs1 = jArrCoordinates.getJSONArray(++index);
+
+                double lat_1 = latlngs1.getDouble(0);
+                double lng_1 = latlngs1.getDouble(1);
+
+                Location simpulTengah = new Location("");
+                simpulTengah.setLatitude(lat_1);
+                simpulTengah.setLongitude(lng_1);
+
+
+                //simpan jarak
+                bobot += simpulAwal.distanceTo(simpulTengah);
+
+                if(index == limit) break; //jika dah smpe ke tengah, break; misal 0-72
+                else --i;
+            }
+        }
+    }
+}
